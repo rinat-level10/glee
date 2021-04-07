aece624c41c9402818b1afc5f572628bc7358f92
rinat-level10 <rinat.mukhtarov@gmail.com>
2021-04-07 17:44:29 +0300

    адаптив

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,10 @@ $(function () {
     arrows: false,
   });
 
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
   var mixer = mixitup('.content__list');
   var mixer = mixitup('.desing-content__list');
 
